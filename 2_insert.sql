7478cc25794b47560df50b1018be5811d36707c9
MecaAlex <118603215+MecaAlex@users.noreply.github.com>
2025-01-12 14:26:01 +0200

    insert dimanic

--- 2_insert.sql
+++ 2_insert.sql
@@ -4,3 +4,4 @@ INSERT INTO tara(nume, indicativ) values('Romania','RO');
 
 --INSERT IN TABLE judete
 INSERT INTO judete(nume, indicativ,tara_fid) values('Timis','TM',1);
+INSERT INTO judete(nume, indicativ,tara_fid) values('Arad','AR', (SELECT fid FROM tara where nume = 'Romania'));
